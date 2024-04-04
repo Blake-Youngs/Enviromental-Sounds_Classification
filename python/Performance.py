649dd57c9a0db69abc5a71bdabd4f035018d671e
Heriberto-Contreras <119393047+Heriberto-Contreras@users.noreply.github.com>
2024-04-03 21:27:03 -0400

    Create Performance.py

---
+++ python/Performance.py
@@ -0,0 +1,40 @@
+import generate_mel_spectrogram as gms
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+
+# Load the trained model
+model = tf.keras.models.load_model('/home/Heriberto/PycharmProjects/Enviromental-Sounds_Classification/model92.5.h5')
+
+# Load the CSV file into a pandas DataFrame
+df = pd.read_csv('/home/Heriberto/PycharmProjects/Enviromental-Sounds_Classification/esc50-10.csv')
+
+# Create a dictionary mapping target labels to categories
+label_to_category = dict(zip(df['target'], df['category']))
+
+# Define your labels
+labels = ['helicopter', 'chainsaw', 'siren', 'car_horn', 'engine', 'train', 'church_bells', 'airplane', 'fireworks', 'hand_saw']
+
+# Need to get the wav file data
+log_mel_spec = gms.log_mel_spectrogram('/home/Heriberto/PycharmProjects/Enviromental-Sounds_Classification/audio/1-172649-A-40.wav', 8000)
+
+# Reshape the input to match the model's input shape
+log_mel_spec = log_mel_spec.reshape(1, log_mel_spec.shape[0], log_mel_spec.shape[1], 1)
+
+# Resize or pad the spectrogram to match the desired shape (60, 61)
+log_mel_spec_resized = np.resize(log_mel_spec, (1, 60, 61, 1))
+
+# Make predictions using the loaded model
+predictions = model.predict(log_mel_spec_resized)
+
+# Get the predicted label index
+predicted_label_index = tf.argmax(predictions[0]).numpy()
+
+# Get the predicted label and category
+predicted_label = labels[predicted_label_index]
+predicted_category = label_to_category.get(predicted_label)
+
+print("Predicted label index:", predicted_label_index)
+print("Label to Category mapping:")
+for label, category in label_to_category.items():
+    print(f"Label: {label}, Category: {category}")
